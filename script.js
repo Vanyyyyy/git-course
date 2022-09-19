7d7bf1560ef17de788f3ec7a44b8c8bbe6e5f85b
Ivan Bashkirov <maxjoker71@gmail.com>
2022-09-19 20:20:08 +0300

    Domashka

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-body.welcome = '<h1>Hello js </h1>'
-console.log('test')
+//№1
+// let str = "Я изучаю JavaScript";
+// console.log(str[2]); // 3 символ
+// console.log(str[0]); // 1 символ
+// console.log(str[str.length-1]); // последний символ
+
+//№2
+// let str = "Я изучаю JavaScript";
+// str = str.replace('ю', 'л');
+// console.log(str)
+// str = str.replace('JavaScript', 'Frontend')
+// console.log(str)
+ 
+
+//№3
+// let arr = ['cat', 'dog', 'parrot', 'horse'];
+// let target = arr.indexOf('parrot');
+// console.log(target);
+
+// №4
+// let arr = ['cat', 'dog', 'parrot', 'horse', 'fish', 'chicken', 'lion'];
+// arr.splice(4, 1);
+// console.log(arr);
+// arr.splice(0, 1);
+// console.log(arr);
+// arr.splice(5, 2)
+// console.log(arr);
+
+// №5
+// let arr = [31, 10, 'chicken', 9, 'fish', 10];
+// // let result = arr.filter(word => word.constructor.name == 'String');
+// // console.log(result);
+// let point = arr.filter(ten => ten == 10);
+// console.log(point);
+
